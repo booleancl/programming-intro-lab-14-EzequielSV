74e20aa5a47c547f59c7950b219930a59a9f5cce
EzequielSV <ezequiel.salazarv@gmail.com>
2022-05-02 20:27:31 -0400

    lab 14 done

--- migrations/4_seedData.sql
+++ migrations/4_seedData.sql
@@ -1,1 +1,6 @@
+\c blog
+
+\COPY users(email) FROM '/home/pi/CursoIntro/programming-intro-lab-14-EzequielSV/data/users.csv' csv HEADER;
+\COPY posts FROM '/home/pi/CursoIntro/programming-intro-lab-14-EzequielSV/data/posts.csv' csv HEADER;
+\COPY comments FROM '/home/pi/CursoIntro/programming-intro-lab-14-EzequielSV/data/comentarios.csv' csv HEADER;
 
